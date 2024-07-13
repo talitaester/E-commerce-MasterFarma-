069de7639934904503d66c6a892c7b3a8bf9c1f8
talitaester <talita.alves@ufba.br>
2024-07-13 20:06:44 -0300

    modified:   e-commerce/src/app/components/Produto/Produto.tsx 	modified:   e-commerce/src/app/gestao/page.tsx 	modified:   e-commerce/src/app/pesquisa/style.css 	modified:   server/prisma/dev.db

--- e-commerce/src/app/components/Produto/Produto.tsx
+++ e-commerce/src/app/components/Produto/Produto.tsx
@@ -1,6 +1,4 @@
-// Em Produto.tsx
-
-'use client'
+'use client';
 
 import React, { useState } from "react";
 import Image from "next/image";
@@ -17,10 +15,7 @@ interface ProdutoProps {
     editable?: boolean;
     code: number;
     onDelete?: () => void;
-}
-
-function excluirProduto() {
-    
+    onEdit?: () => void; // Adicionando a prop onEdit
 }
 
 const Produto: React.FC<ProdutoProps> = ({
@@ -31,21 +26,19 @@ const Produto: React.FC<ProdutoProps> = ({
     imagemSrc = "/produto.png",
     editable = false,
     code,
-    onDelete, // Função para deletar o produto
+    onDelete,
+    onEdit,
 }) => {
     const [isEditVisible, setIsEditVisible] = useState(false);
-    const handleEdit = (nome: string) => {
-        console.log(`Editing product: ${nome}`);
-        // Lógica de edição se necessário
-    };
 
-    const handleDelete = async (nome: string) => {
+    const handleDelete = async () => {
         try {
             await axios.delete(`http://localhost:8080/products/code/${code}`);
             console.log(`Deleted product: ${nome}`);
             if (onDelete) {
                 onDelete(); // Chama a função onDelete para atualizar a lista de produtos
             }
+            setIsEditVisible(false); // Fecha o formulário de confirmação
         } catch (error) {
             console.error("Erro ao deletar produto:", error);
         }
@@ -58,14 +51,14 @@ const Produto: React.FC<ProdutoProps> = ({
                     className={styles.imagemProduto}
                     src={imagemSrc}
                     alt={nome}
-                    width="258"
-                    height="258"
+                    width={258}
+                    height={258}
                 />
                 
                 <div className={styles.container}>
                     <h6 className={styles.nomeProduto}>{nome}</h6>
 
-                        <div className={styles.precos}>
+                    <div className={styles.precos}>
                         <span className={styles.corte}></span>
                         <h6 className={styles.nomeProduto}>{precoAntigo}</h6>
 
@@ -76,23 +69,26 @@ const Produto: React.FC<ProdutoProps> = ({
 
                 {editable && (
                     <div className={styles.edit}>
-                            <>
-                            <button className={styles.editButton}>
-                            <h6 className={styles.editName}>Editar</h6>
+                        <>
+                            <button className={styles.editButton} onClick={onEdit}>
+                                <h6 className={styles.editName}>Editar</h6>
                             </button>
                             <button className={styles.delete} onClick={() => setIsEditVisible(true)}>
-                                <Image className={styles.lixo} src='/mini-lixeira.svg'alt="deletar" width={24} height={28}/>
+                                <Image className={styles.lixo} src='/mini-lixeira.svg' alt="deletar" width={24} height={28} />
                             </button>
-                            </>
+                        </>
                     </div>
                 )}
             </div>
             {isEditVisible && (
                 <div className={styles.excluirCerteza}>
-                    <h6>Tem certeza que<br/>quer excluir?</h6>
+                    <h6>Tem certeza que<br />quer excluir?</h6>
                     <div className={styles.botoesEdit}>
                         <button className={styles.botaoNao} onClick={() => setIsEditVisible(false)}><h6>Não</h6></button>
-                        <button className={styles.botaoSim} onClick={() => excluirProduto()}><Image className={styles.lixo} src='/mini-lixeira.svg'alt="deletar" width={24} height={28}/><h6>Sim</h6></button>
+                        <button className={styles.botaoSim} onClick={handleDelete}>
+                            <Image className={styles.lixo} src='/mini-lixeira.svg' alt="deletar" width={24} height={28} />
+                            <h6>Sim</h6>
+                        </button>
                     </div>
                 </div>
             )}
